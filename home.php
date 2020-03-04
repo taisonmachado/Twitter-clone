a952622df4874442f6673dca5063c094f134bb8c
taisonmachado <taison.machado@hotmail.com>
2020-03-04 10:54:26 -0300

    módulo de incluir tweets

--- home.php
+++ home.php
@@ -1,74 +1,136 @@
 <?php
 
-    session_start();
+session_start();
 
-    if(!isset($_SESSION['usuario'])){
-        header('Location: index.php?erro=1');
-    }
+if (!isset($_SESSION['usuario'])) {
+	header('Location: index.php?erro=1');
+}
 
 ?>
 
 <!DOCTYPE HTML>
 <html lang="pt-br">
-	<head>
-		<meta charset="UTF-8">
 
-		<title>Twitter clone</title>
-		
-		<!-- jquery - link cdn -->
-		<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+<head>
+	<meta charset="UTF-8">
 
-		<!-- bootstrap - link cdn -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-	
-	</head>
+	<title>Twitter clone</title>
 
-	<body>
+	<!-- jquery - link cdn -->
+	<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
 
-		<!-- Static navbar -->
-	    <nav class="navbar navbar-default navbar-static-top">
-	      <div class="container">
-	        <div class="navbar-header">
-	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-	            <span class="sr-only">Toggle navigation</span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	          </button>
-	          <img src="imagens/icone_twitter.png" />
-	        </div>
-	        
-	        <div id="navbar" class="navbar-collapse collapse">
-	          <ul class="nav navbar-nav navbar-right">
-	            <li><a href="sair.php">Sair</a></li>
-	          </ul>
-	        </div><!--/.nav-collapse -->
-	      </div>
-	    </nav>
+	<!-- bootstrap - link cdn -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
+	<script type="text/javascript">
+		$(document).ready(function() {
 
-	    <div class="container">
-	    	
-	    	<br /><br />
+			//associar o evento ao click do botao.
+			$('#btn_tweet').click(function() {
 
-	    	<div class="col-md-4"></div>
-	    	<div class="col-md-4">
-	    		Usuário Autenticado !!
+				if ($('#texto_tweet').val().length > 0) {
+
+					$.ajax({
+						url: 'inclui_tweet.php',
+						method: 'post',
+						data: $('#form_tweet').serialize(),
+						success: function(data) {
+							$('#texto_tweet').val('');
+							atualizaTweets();
+						}
+					});
+
+				}
+
+			});
+
+			function atualizaTweets(){
+				$.ajax({
+					url: 'get_tweet.php',
+					success: function(data){
+						$('#tweets').html(data);
+					}
+				});
+			}
+
+			atualizaTweets();
+
+		});
+	</script>
+
+</head>
+
+<body>
+
+	<!-- Static navbar -->
+	<nav class="navbar navbar-default navbar-static-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<img src="imagens/icone_twitter.png" />
 			</div>
-			<div class="col-md-4"></div>
 
-			<div class="clearfix"></div>
-			<br />
-			<div class="col-md-4"></div>
-			<div class="col-md-4"></div>
-			<div class="col-md-4"></div>
+			<div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="sair.php">Sair</a></li>
+				</ul>
+			</div>
+			<!--/.nav-collapse -->
+		</div>
+	</nav>
 
+
+	<div class="container">
+		<div class="col-md-3">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<h4> <?= $_SESSION['usuario'] ?> </h4>
+					<hr />
+					<div class="col-md-6">
+						<h5>Tweets</h5>
+					</div>
+					<div class="col-md-6">
+						<h5>Seguidores</h5>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-6">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<form id="form_tweet" class="input-group">
+						<input type="text" placeholder="Insira o tweet aqui" name="texto_tweet" id="texto_tweet" class="form-control" maxlength="140">
+						<span class="input-group-btn">
+							<button class="btn btn-default" id="btn_tweet" type="button">Tweet</button>
+						</span>
+					</form>
+				</div>
+			</div>
+			
+			<div id="tweets" class="list-group"></div>
 		</div>
 
 
-	    </div>
-	
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-	
-	</body>
+		<div class="col-md-3">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<h4> <a href="#">Procurar Pessoas</a> </h4>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	</div>
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+</body>
+
 </html>
